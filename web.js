a7e457697d9c38ed3a7f5497fea4263607054f7d
Amitabh Sharma <sharma.amitabh@gmail.com>
2013-07-06 16:06:39 +0530

    using readFileSync and Buffer.toString()

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readhtm =  function(pagename){
+	var buffer = new Buffer(fs.readFileSync(pagename));
+	return buffer.toString("utf-8");
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(readhtm("index.html"));
 });
 
 var port = process.env.PORT || 5000;
